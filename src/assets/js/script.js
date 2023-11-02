34c52b106ee5164b48a76c6faf5895e22efd7218
2883857@student.edu.az <2883857@student.edu.az>
2023-11-02 19:17:44 +0400

    dark mode onclick

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,20 +1,38 @@
 $('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:350,
-    nav:true,
-    autoplay:true,
+    loop: true,
+    margin: 350,
+    nav: true,
+    autoplay: true,
     dots: true,
     autoplayTimeout: 1000,
-    
-    responsive:{
-        0:{
-            items:1
+
+    responsive: {
+        0: {
+            items: 1
         },
-        600:{
-            items:3
+        600: {
+            items: 3
         },
-        1000:{
-            items:5
+        1000: {
+            items: 5
         }
     }
 })
+
+
+
+
+// function myDark() {
+
+//     let ht = document.querySelector("#ht");
+
+//     ht.classList.add("dark");
+// }
+
+
+
+function myDark() {
+    !document.querySelector("#ht").classList.contains('dark') ? ht.classList.add('dark') : ht.classList.remove('dark');
+}
+
+myDark();
